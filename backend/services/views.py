b8853e3ba88fe1c93d67269224bd92ca1bb99852
ruggieroDaniela <chachy.drs@gmail.com>
2023-05-10 20:08:19 -0400

    autentication

--- backend/services/views.py
+++ backend/services/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .models import Services
 from .serializers import ServicesSerializer
 
@@ -9,6 +11,9 @@ from .serializers import ServicesSerializer
 class ServicesViewSet(viewsets.ModelViewSet):
     queryset = Services.objects.all()
     serializer_class = ServicesSerializer
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     @action(detail=False, methods=['post'])
     def post_ad(self, request):
