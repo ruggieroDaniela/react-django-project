cdd0ce68e53d091b7ff6047a49e3dd9fa0085911
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-05-12 22:51:59 -0400

    include login json into i18n setup

--- frontend/ati-2-frontend/src/i18n.js
+++ frontend/ati-2-frontend/src/i18n.js
@@ -6,18 +6,21 @@ import navbar_en from './locales/en/Navbar.json';
 import registrar_en from "./locales/en/Registrar.json";
 import multiform_en from "./locales/en/Multiform.json";
 import general_en from "./locales/en/General.json";
+import login_en from "./locales/en/IniciarSesion.json"
 
 import navbar_es from './locales/es/Navbar.json';
 import registrar_es from "./locales/es/Registrar.json";
 import multiform_es from "./locales/es/Multiform.json";
 import general_es from "./locales/es/General.json";
+import login_es from "./locales/es/IniciarSesion.json"
 
 const en = Object.assign(
   {},
   navbar_en,
   registrar_en,
   multiform_en,
-  general_en
+  general_en,
+  login_en
 );
 
 const es = Object.assign(
@@ -25,7 +28,8 @@ const es = Object.assign(
   navbar_es,
   registrar_es,
   multiform_es,
-  general_es
+  general_es,
+  login_es
 );
 
 i18n
