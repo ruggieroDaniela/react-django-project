c6e3bc2a44598d719725974a55a6d2364b782150
Hadaya Hadaya <hadaya456@gmail.com>
2023-05-12 20:20:55 -0400

    arreglados detalles de la internacionalizacion

--- frontend/ati-2-frontend/src/routes/IniciarSesion.jsx
+++ frontend/ati-2-frontend/src/routes/IniciarSesion.jsx
@@ -6,42 +6,42 @@ export const IniciarSesion = () => {
     
     const { t, i18n } = useTranslation();
 
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('submit exitoso', email, password)
+        //console.log(email, password)
     }
 
 
   return (
     <div className='container'>
-    <div className='login'>
-        <h2 className='title'>{t('login.titulo')}</h2>
+        <div className='login'>
+            <h2 className='title'>{t('login.titulo')}</h2>
 
-        <form onSubmit={handleSubmit} className='form'>
-            <p>{t('login.descripcion')}</p>
+            <form onSubmit={handleSubmit} className='form'>
+                <p>{t('login.descripcion')}</p>
 
-            <div>
                 <div>
-                    <label id="input-login">{t('login.correo')}</label>
-                    <input type="text" onChange={e => setEmail(e.target.value)} required />
+                    <div>
+                        <label id="input-login">{t('login.correo')} </label>
+                        <input type="text" onChange={e => setEmail(e.target.value)} required />
+                    </div>
+                    <div>
+                        <label>{t('login.contrasena')} </label>
+                        <input type="password" onChange={e => setPassword(e.target.value)} required />
+                    </div>
                 </div>
-                <div>
-                    <label>{t('login.contrasena')} </label>
-                    <input type="password" onChange={e => setPassword(e.target.value)} required />
-                </div>
-            </div>
 
-            
-            <button type="submit">{t('login.boton')}</button>
-            
-            <a href="#">{t('login.olvide_contrasena')}</a>
+                
+                <button type="submit">{t('login.boton')}</button>
+                
+                <a href="#">{t('login.olvide_contrasena')}</a>
 
-        </form>
-        
-    </div>
+            </form>
+            
+        </div>
     </div>
   )
 }
