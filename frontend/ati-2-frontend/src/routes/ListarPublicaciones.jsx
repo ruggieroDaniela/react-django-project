0f5cefc183865988710121394a70ce7eb46b306f
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-05-29 19:12:08 -0400

    basic input menu without the posts

--- frontend/ati-2-frontend/src/routes/ListarPublicaciones.jsx
+++ frontend/ati-2-frontend/src/routes/ListarPublicaciones.jsx
@@ -3,6 +3,8 @@ import { useTranslation } from 'react-i18next'
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
+import "../styles/ListarPublicaciones.scss"
+
 export const ListarPublicaciones = () => {
 
     const {t} = useTranslation();
@@ -10,6 +12,10 @@ export const ListarPublicaciones = () => {
     const searchParams = location.search;
 
     const [postList, setPostList] = useState([]);
+
+    const [selectedTipoPersona, setSelectedTipoPersona] = useState("");
+    const tipoPersona = ["1", "2", "3", "4", "5"];
+    const ordenes = ["1", "2", "3", "4"];
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -32,7 +38,70 @@ export const ListarPublicaciones = () => {
     console.log(searchParams);
     return(<>
         <div id="lista-posts">
+            <div className="header">
+                Publicaciones Realizadas
+            </div>
+            <div className="row">
+                <span className="title">
+                    Ver listado como:
+                </span>
+                <ul className="input-group">
+                    <li className="radio">
+                        <label>
+                            <input type="radio"/>
+                            lista
+                        </label>
+                    </li>
+                    <li className="radio">
+                        <label>
+                            <input type="radio"/>
+                            foto
+                        </label>
+                    </li>
+                </ul>
+            </div>
             
+            <div className="row note">
+                seleccione
+            </div>
+
+            <div className="row">
+                <span className="title">
+                    Tipo de persona:
+                </span>
+                <ul className="input-group">
+                    {
+                        tipoPersona.map( (item, i) => 
+                            <li className="checkbox">
+                                <label>
+                                    <input type="checkbox"/>
+                                    <div className="checkbox-label">
+                                        {item}
+                                    </div>
+                                </label>
+                            </li>
+                        )
+                    }
+                </ul>
+            </div>
+
+            <div className="row">
+                <span className="title">
+                    Ordenar:
+                </span>
+                <ul className="input-group">
+                    {
+                        ordenes.map( (item, i) => 
+                            <li className="button">
+                                <button>
+                                        {item}
+                                </button>
+                            </li>
+                        )
+                    }
+                </ul>
+            </div>
+
         </div>
     </>);
 
