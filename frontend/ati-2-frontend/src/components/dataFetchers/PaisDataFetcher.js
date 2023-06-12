bf52e989d6535535b2952f7bf338f17e74d6db74
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-06-12 17:58:41 -0400

    getStatesInCountry

--- frontend/ati-2-frontend/src/components/dataFetchers/PaisDataFetcher.js
+++ frontend/ati-2-frontend/src/components/dataFetchers/PaisDataFetcher.js
@@ -1,4 +1,4 @@
-import countries from "../../data/countries.json"
+import data from "../../data/countries.json"
 import axios from 'axios';
 
 const API_KEY = "M3F5RW5Hb1dkWFpNN2kwN1k1eEhNYlRYZUJuQW5Wb3NETlF6YTd5cg=="
@@ -11,9 +11,9 @@ export const getCountryName = (countryCode, lang = "en") => {
 
     for (let i = 0; i < continents.length; i++) {
         
-        if( countryCode in countries[ continents[i] ] ){
-            console.log(countries[ continents[i] ][countryCode][lang] );
-            return countries[ continents[i] ][countryCode][lang];
+        if( countryCode in data[ continents[i] ] ){
+            console.log(data[ continents[i] ][countryCode][lang] );
+            return data[ continents[i] ][countryCode][lang];
         }
         
     }
@@ -28,9 +28,9 @@ export const getAllCountries = (lang = "en") => {
 
     for (let i = 0; i < continents.length; i++) {
         
-        Object.keys( countries[ continents[i] ] )
+        Object.keys( data[ continents[i] ] )
             .forEach(code => {
-                names.push( countries[ continents[i] ][code][lang] );
+                names.push( data[ continents[i] ][code][lang] );
                 values.push(code);
             });
         
@@ -48,79 +48,41 @@ export const getCountriesInRegion = (reg, lang="en") => {
     const names = []
     const values = []
         
-    Object.keys( countries[reg] )
+    Object.keys( data[reg] )
         .forEach(code => {
-            names.push( countries[reg][code][lang] );
+            names.push( data[reg][code][lang] );
             values.push(code);
         });
-        
-    
 
     return [names, values]
 
 };
 
-export const getStatesInCountry = async (countries) => {
-    try {
+export const getStatesInCountry = (countries, lang="en") => {
 
-        let response;
+    const names = [];
+    const values = [];
 
-        const names = [];
-        const values = [];
-
-        const codes = countries.split(",");
-        for (let i = 0; i < codes.length; i++) {
-            if(codes[i].length > 0 ){
-                response = await axios.get(`https://api.countrystatecity.in/v1/countries/${codes[i]}/states`, {
-                    headers: {
-                        'X-CSCAPI-KEY': API_KEY
+    const codes = countries.split(",");
+    
+    for(const countryCode of codes){
+        for(const reg of continents){
+            if( countryCode in data[reg] ){
+                Object.keys( data[reg][countryCode]["states"] ).forEach(
+                    stateCode => {
+                        values.push(`${countryCode}-${stateCode}`);
+                        names.push(data[reg][countryCode]["states"][stateCode]["name"])
                     }
-                });
-
-                for (let j = 0; j < response.data.length; j++) {
-                    names.push( response.data[j].name );
-                    values.push( `${codes[i]}-${response.data[j].iso2}` );
-                }
+                )
+                break;
             }
-
         }
-
-        return [names, values];
-        
-    } catch (error) {
-        console.error(error);
     }
+
+    return [names, values];
+
 };
 
-export const getStatesInOneCountry = async (countries) => {
-    try {
-
-        let response;
-
-        const names = [];
-        const values = [];
-
-        const codes = countries.split(",");
-        for (let i = 1; i < codes.length; i++) {
-            response = await axios.get(`https://api.countrystatecity.in/v1/countries/${codes[i]}/states`, {
-                headers: {
-                    'X-CSCAPI-KEY': API_KEY
-                }
-            });
-
-            for (let j = 0; j < response.data.length; j++) {
-                names.push( response.data[j].name );
-                values.push( `${codes[i]}-${response.data[j].iso2}` );
-            }
-
-        }
-
-        return [names, values];
-        
-    } catch (error) {
-        console.error(error);
-    }
-};
 
 export const getCitiesInStates = async (stateCodes) => {
     try {
