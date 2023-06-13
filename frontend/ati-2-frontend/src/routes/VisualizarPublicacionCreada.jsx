1f11da06112cce2851c4d71dd383352ab81f08cc
ruggieroDaniela <chachy.drs@gmail.com>
2023-06-12 20:35:54 -0400

    lugar de procedencia

--- frontend/ati-2-frontend/src/routes/VisualizarPublicacionCreada.jsx
+++ frontend/ati-2-frontend/src/routes/VisualizarPublicacionCreada.jsx
@@ -129,107 +129,54 @@ export const VisualizarPublicacionCreada = () => {
 
                 <br></br>
 
+                {/* LUGAR DE PROCEDENCIA */ }
+                <div className='basico'>
+                    <div className='rectangle blue tag'>{t('publicacionCreada.lugar_procedencia_cuidador')}</div>                    
+                </div>
+                <br></br>
+
+                { /* País de procedencia */ }
+                <div className='basico'>
+                    <div className='basico info'>
+                        <div className='subtitle black'><b>{t('publicacionCreada.pais_cuidador')}</b></div>
+                        <div> { data?.country || "Venezuela"}  </div>
+                    </div>
+                </div>
+                
+                { /* Estado / Provincia  */ }
+                <div className='basico'>
+                    <div className='basico info'>
+                        <div className='subtitle black'><b>{t('publicacionCreada.provincia__cuidador')}</b></div>
+                        <div>{data?.state || "Distrito Capital"}</div>
+                    </div>
+                </div>
+
+                { /* Ciudad */ }
+                <div className='basico'>
+                    <div className='basico info'>
+                        <div className='subtitle black'><b>{t('publicacionCreada.ciudad_cuidador')}</b></div>
+                        <div>{data?.city || "Caracas"} </div>
+                    </div>
+                </div>
+
+                { /* Zona */ }
+                <div className='basico'>
+                    <div className='basico info'>
+                        <div className='subtitle black'><b>{t('publicacionCreada.zona_cuidador')}</b></div>
+                        <div>{data?.zone || "Candelaria"}</div>
+                    </div>
+                </div>
+
+                <br></br>
+
                 
                 {/*        
-                <section className='encabezado-perfil'>
-                <section>
-                        <section className='foto'>
-                            <img
-                                src={fotoPerfil}
-                                style={{width:'30%', height:'30%'}}
-                            />
-                        </section>
 
-                </section>
-                <section style={{width:'100%'}}> 
-                        <h1>Ana Peres</h1>
-                        <section className='informacion-perfil'>
-                            <span className='titulo'> {t('publicacionCreada.telefono_celular')}</span>
-                            <span> { data?.phone || '0412-123.32.23'} </span>
-                        </section>
-                        <section className='informacion-perfil'>
-                            <span className='titulo'> {t('publicacionCreada.telefono_fijo')}</span>
-                            <span> {data?.phone || '0412-123.32.23'} </span>
-                        </section>
-                        <section className='informacion-perfil'>
-                            <span className='titulo'> {t('publicacionCreada.correo_electronico')}</span>
-                            <span>{data?.email ||'mariaf0821@gmail.com'}</span>
-                        </section>
 
-                </section>
-                <section> 
-
-                        <section className='informacion-pais'>
-                            <span className='span-01'> {t('publicacionCreada.pais')}</span>
-                            <span>  0412-123.32.23</span>
-                        </section>
-                        <section className='informacion-pais'>
-                        <span className='span-01'> {t('publicacionCreada.provincia')}</span>
-                            <span>  0412-123.32.23</span>
-                        </section>
-                </section>
                 </section>
                 {
                     <>
-                        <section className='container-01'>
-                            <section className='informacion-basica'>
-                                <section className='titulo-02'>
-                                    <h1 > {t('publicacionCreada.datos_basicos_cuidador')}</h1>
-                                </section>
-
-                                <section>
-                                    <p>
-                                        {
-                                            data?.description ||
-                                           "Soy persona de buena presencia, responsable, honesta, buen carácter, y que le  gusta trabajar cuidando personas"
-                                        }
-                                    </p>
-                                </section>
-                                <section className='container-span'>
-                                    <span className='span-01'> {t('publicacionCreada.edad_cuidador')}</span>
-                                    <span>  { data?.data || "Entre 20 y 30 años / Con hijos"} </span>
-                                </section>
-                                <section className='container-span'>
-                                    <span className='span-01'> {t('publicacionCreada.sexo_cuidador')}</span>
-                                    <span>  { data?.data || "Masculino" }</span>
-                                </section>
-                                <section className='container-span'>
-                                    <span className='span-01'> {t('publicacionCreada.situcion_familiar_cuidador')} </span>
-                                    <span> {data?.have_children ? "Con hijos" : "Sin hijos" || 'con hijos'}</span>
-                                </section>
-                                <section className='container-span'>
-                                    <span className='span-01'> {t('publicacionCreada.grado_instrucción_cuidador')} </span>
-                                    <span>  {data?.education_level || "Técnico Univeristario"} </span>
-                                </section> 
-                            </section>
-
-                            <section className='informacion-basica'>
-                                <section className='titulo-02'>
-                                        <h1> {t('publicacionCreada.lugar_procedencia_cuidador')}</h1>
-                                </section>
-                                <p>
-                                    <br></br>
-                                </p>
-                
-                                <section className='container-span'>
-                                    <span className='titulo'>{t('publicacionCreada.pais_cuidador')}</span>
-                                    <span>{ data?.country || "Venezuela"} </span>
-                                </section>
-                                <section className='container-span'>
-                                    <span className='titulo'>{t('publicacionCreada.provincia_cuidador')} </span>
-                                    <span> {data?.state || "Distrito Capital"}</span>
-                                </section>
-                                <section className='container-span'>
-                                    <span className='titulo'>{t('publicacionCreada.ciudad_cuidador')}  </span>
-                                    <span> {data?.city || "caracas"} </span>
-                                </section>
-                                <section className='container-span'>
-                                    <span className='titulo'>{t('publicacionCreada.zona_cuidador')} </span>
-                                    <span> {data?.zone || "Candelaria"}</span>
-                                </section>
-                                
-                            </section>
-                        </section>
+        
 
                         <section className='container-01'>
                             <section className='informacion-basica'>
