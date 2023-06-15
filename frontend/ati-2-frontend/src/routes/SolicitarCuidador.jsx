67781bf832e96b3aa665486b6e5e7c57217c38e1
gabrielLiendo <gabo.c.liendo@gmail.com>
2023-06-15 14:37:19 -0400

    json error solicitar

--- frontend/ati-2-frontend/src/routes/SolicitarCuidador.jsx
+++ frontend/ati-2-frontend/src/routes/SolicitarCuidador.jsx
@@ -60,7 +60,7 @@ export const SolicitarCuidador = () => {
     //let stagesNames = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
     let stagesNames = [];
     for (let i = 0; i < FasesSolicitarCuidador.length; i++) {
-        stagesNames.push(t('SolicitarCuidador.fases.'+i+'.nombre'));
+        stagesNames.push(t('SolicitarNiñera.fases.'+i+'.nombre'));
     }
 
     return (
